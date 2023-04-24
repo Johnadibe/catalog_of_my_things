30635b65a36f514b8f8de9f86e2f4b3dbb603e95
John Adibe <johnadibe123@gmail.com>
2023-04-24 14:31:30 -0800

    Add methods implementation in item.rb file

--- item.rb
+++ item.rb
@@ -1,22 +1,35 @@
+require 'date'
 
 class Item
-  attr_accessor :id, :genre, :author, :source, :label, :publish_date
+  attr_accessor :publish_date
+  attr_reader :id, :archived, :genre, :author, :label
 
-  def initialize(id, genre, author, source, label, publish_date )
-    @id = id
-    @genre = genre
+  def initialize(publish_date)
+    @id = Random.rand(1..1000)
+    @publish_date = Date.parse(publish_date)
+    @archived: false
+  end
+
+  def add_author(author)
+    author.add_items << self unless author.items.include? self
     @author = author
-    @source = source
+  end
+
+  def add_genre(genre)
+    genre.add_items << self unless genre.items.include? self
+    @genre = genre
+  end
+
+  def add_label(label)
+    @label.add_items << self unless label.items.include? self
     @label = label
-    @publish_date = publish_date
-    @archived: boolean
   end
 
   def can_be_archived?
-
+    true if (DateTime.now - @publish_date).to_i / 365 > 10
   end
 
   def move_to_archive
-
+    @archived = true if can_be_archived?
  end
 end
--- main.rb
+++ main.rb
@@ -1,16 +1,13 @@
 options = {
   '1' => 'List all books',
   '2' => 'List all music albums',
-  '3' => 'List all movies',
-  '4' => 'List all games',
-  '5' => 'List all genres',
-  '6' => 'List all labels',
-  '7' => 'List all authors',
-  '8' => 'List all sources',
-  '9' => 'Add a book',
-  '10' => 'Add a music album',
-  '11' => 'Add a movie',
-  '12' => 'Add a game',
+  '3' => 'List all games',
+  '4' => 'List all genres',
+  '5' => 'List all labels',
+  '6' => 'List all authors',
+  '7' => 'Add a book',
+  '8' => 'Add a music album',
+  '9' => 'Add a game',
   '0' => 'Exit App'
 }
 
