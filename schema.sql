348c6fc40e0a7dd9cbceaa182591839c00ac15d6
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-04-25 22:13:42 +0500

    updated schema.sql

--- schema.sql
+++ schema.sql
@@ -1,53 +1,53 @@
-CREATE TABLE Items (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
+CREATE TABLE items (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     genre_id INT,
     author_id INT,
     label_id INT,
-    publish_date DATE
+    publish_date DATE,
     archived BOOLEAN,
-    FOREIGN KEY (genre_id) REFERENCES Genre(id)
+    FOREIGN KEY (genre_id) REFERENCES genres(id)
 );
 
-create table Authors (
-    id int generated always as identity,
-    first_name char(30),
-    last_name char(30),
-    item_id int,
-    foreign key (item_id) references Item(id)
-)
+CREATE TABLE authors (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    item_id INT,
+    FOREIGN KEY (item_id) REFERENCES items(id)
+);
 
-create table Games (
-    id int,
-    last_played_at datetime,
-    multiplayer boolean,
-    foreign key (id) references Item(id)
-)
+CREATE TABLE games (
+    id INT PRIMARY KEY,
+    last_played_at TIMESTAMP,
+    multiplayer BOOLEAN,
+    FOREIGN KEY (id) REFERENCES items(id)
+);
 
-CREATE TABLE Music (
-    id INT,
-    name VARCHAR(100),
+CREATE TABLE music (
+    id INT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
     on_spotify BOOLEAN,
-    FOREIGN KEY (id) REFERENCES Item(id)
+    FOREIGN KEY (id) REFERENCES items(id)
 );
 
-CREATE TABLE Genres (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
+CREATE TABLE genres (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE Books (
-  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  publisher VARCHAR(50),
-  cover_state VARCHAR(50),
-  publish_date DATE NOT NULL,
-  archived BOOLEAN NOT NULL,
-  label_id INT REFERENCES labels(id)
-   genre_id INTEGER REFERENCES genres(id),
-  author_id INTEGER REFERENCES authors(id),
+CREATE TABLE books (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    publisher VARCHAR(50) NOT NULL,
+    cover_state VARCHAR(50) NOT NULL,
+    publish_date DATE NOT NULL,
+    archived BOOLEAN NOT NULL,
+    label_id INT REFERENCES labels(id),
+    genre_id INT REFERENCES genres(id),
+    author_id INT REFERENCES authors(id)
 );
 
-CREATE TABLE Labels (
-  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  title VARCHAR(250),
-  color VARCHAR(50)
+CREATE TABLE labels (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title VARCHAR(250) NOT NULL,
+    color VARCHAR(50) NOT NULL
 );
