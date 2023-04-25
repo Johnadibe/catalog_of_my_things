f05f30a3c0e46bb27b1081f36afd6f1a6f7299fd
John Adibe <johnadibe123@gmail.com>
2023-04-25 15:50:07 -0800

    Fix linter errors

--- spec/music_spec.rb
+++ spec/music_spec.rb
@@ -2,11 +2,11 @@ require_relative '../music'
 require_relative '../item'
 
 describe Music do
-    before :each do
-      @music = Music.new(true)
-    end
+  before :each do
+    @music = Music.new(true)
+  end
 
-    it 'check if the music is on spotify' do
-        expect(@music.on_spotify).to eq(true)
-    end
+  it 'check if the music is on spotify' do
+    expect(@music.on_spotify).to eq(true)
+  end
 end
