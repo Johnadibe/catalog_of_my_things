6f107fec0c08fecb99b6e07b47c34cfe9d750783
Donmark2k <chukwuemekavp@gmail.com>
2023-04-25 12:08:57 +0100

    Created a schema for book and label

---
+++ book-label-schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE books (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  publisher VARCHAR(50),
+  cover_state VARCHAR(50),
+  publish_date DATE NOT NULL,
+  archived BOOLEAN NOT NULL,
+  label_id INT REFERENCES labels(id)
+);
+
+CREATE TABLE labels (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title VARCHAR(250),
+  color VARCHAR(50)
+);
